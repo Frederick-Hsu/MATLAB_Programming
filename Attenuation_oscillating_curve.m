931e96889abc0bdbea6f72a1d6f8d3f3bbb52870
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-01-31 00:37:29 +0800

    Remove the Chinese gibberish from the comment.

--- Attenuation_oscillating_curve.m
+++ Attenuation_oscillating_curve.m
@@ -1,7 +1,7 @@
-t = 0:pi/50:4*pi;           % 自变量t的取值范围
-y = exp(-t/3).*sin(3*t);    % 计算与自变量相应的y数组。 注意：乘法符号前面的小黑点
+t = 0:pi/50:4*pi;          
+y = exp(-t/3).*sin(3*t);   
 
-plot(t, y, '-r', 'LineWidth', 2)    % 绘制衰减振荡曲线
+plot(t, y, '-r', 'LineWidth', 2)   
 axis([0, 4*pi, -1, 1])
 xlabel('t')
 ylabel('y')
