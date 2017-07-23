ce6d39097b29561dc1ceb07bd2d890ad72474efc
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-07-23 20:34:46 +0800

    Make a comprehensive comparison between iterative computation and perception network computation,
    to check how the perception network solve the linear speration problem.

--- Neural_Network/perception_classifier.m
+++ Neural_Network/perception_classifier.m
@@ -9,14 +9,15 @@ Weights = [0, 0, 0];   % Initial weights [b, w1, w2]
 
 sample_points = [-9,  1, -12, -4,  0, 5;
                  15, -8,   4,  5, 11, 9];
+% expectation = [1, 1, 0, 0, 0, 1];
 expectation = [0, 1, 0, 0, 0, 1];
 
 sample_points = [ones(1, 6); sample_points];
-MAX_ITERATION_COUNT = 20;
+MAX_ITERATION_COUNT = 180;
 
 %%
 % Train the perception network
-
+error = -1 * ones(1, 200);
 i = 0;
 while 1
     v = Weights * sample_points;
@@ -45,8 +46,10 @@ end
 % Visualize the binary classification result
 
 figure(1);
+% scatter([-12, -4, 0], [4, 5, 11], 'r*');
 scatter([-9, -12, -4, 0], [15, 4, 5, 11], 'r*');
 hold on;
+% scatter([-9, 1, 5], [15, -8, 9], 'b^');
 scatter([1, 5], [-8, 9], 'b^');
 
 axis([-14, 8, -10, 16]);
@@ -61,9 +64,34 @@ grid on;
 
 figure(2)
 x = 0 : i;
-plot(x, error, 'o-');
+plot(x, error(1:i+1), 'o-');
 grid on;
 xlabel('iteration count');
 ylabel('Mean Absolute Error');
 string = sprintf('MAE value (iteration count : %d)', i+1);
 title(string);
+
+
+%%
+
+net = newp([-20, 20; -20, 20], 1);    % Create the perception network
+net = train(net, sample_points(2:3, :), expectation);    % Train the network
+output = sim(net, sample_points(2:3, :))    % Expectation output
+
+temp_weights = net.IW;
+temp_bias = net.b;
+network_weights = [temp_bias{1}, temp_weights{1}]
+
+figure(3);
+scatter([-9, -12, -4, 0], [15, 4, 5, 11], 'r*');
+hold on;
+scatter([1, 5], [-8, 9], 'b^');
+
+hold on;
+separator_line_x = -12:1:6;
+separator_line_y = (-network_weights(1) - network_weights(2)*separator_line_x)/network_weights(3);
+plot(separator_line_x, separator_line_y, 'g-.');
+
+grid on;
+title('Perception network to classify the linear separation');
+legend('class 0', 'class 1', 'spearator line');
